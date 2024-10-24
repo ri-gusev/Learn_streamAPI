e90544996bf111332bb65b1567489107cb4143a2
ri-gusev <gusev.roman1@mail.ru>
2024-10-24 12:28:17 +0300

    терминальные и промежуточные операторы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,26 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<User> array = new ArrayList<>();
 
-        List<Integer> array = new ArrayList<>();
+        array.add(new User("Vasya",15));
+        array.add(new User("Roma",16));
+        array.add(new User("Ivan",20));
+        array.add(new User("Lena",16));
+        array.add(new User("Varya",54));
+        array.add(new User("Alex",23));
+        array.add(new User("John",67));
+        array.add(new User("Richard",52));
 
-        for (int i = 0; i < 100; i++) {
-         array.add((int)(Math.random() * 1000));
-        }
-
-        List<Integer> newArr = myFilter(array, (n) -> n % 2 == 0);
-
-        System.out.println(newArr);
-
-    }
-
-    private static List<Integer> myFilter(List<Integer> src, MyPredicate predicate){
-        List<Integer> result = new ArrayList<>();
-        for (Integer num : src) {
-            if (predicate.test(num)) {
-                result.add(num);
-            }
-        }
-        return result;
+        array.stream()
+                .filter(user -> user.getAge() < 40)
+                .sorted(Comparator.comparing(User::getName))
+                .limit(3)
+                .map(User::getName)
+                .forEach(System.out::println);
 
     }
 
